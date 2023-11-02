6130072c415b062bbd5471e5eb9e94080cc99298
ThommysArt <keabou.thomson@ictuniversity.edu.cm>
2023-11-02 10:37:36 +0100

    changed dir out of portfolio

--- src/components/custom/SkillBox.jsx
+++ src/components/custom/SkillBox.jsx
@@ -1,11 +1,11 @@
-import { Avatar, Box, Text, Flex } from "@radix-ui/themes"
+import { Avatar, Box, Text, Flex, Strong } from "@radix-ui/themes"
 
 export const SkillBox = ({skill, icon, fallback}) => {
     return (
         <Box style={{backgroundColor: '#D6E1FF', borderRadius: '7px', padding: '5%'}}>
             <Flex gap='3'>
                 <Avatar src={icon} fallback={fallback} />
-                <Text>{skill}</Text>
+                <Text><Strong>{skill}</Strong></Text>
             </Flex>
         </Box>
     )
